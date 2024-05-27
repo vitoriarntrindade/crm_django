2bad4b2035dc0e287c7bd95ddaa38474dfe6e1f6
vitoria <vitoriarntrindade@gmail.com>
2024-05-27 09:42:22 -0400

    view para update/patch configurada

--- crm/customer/urls.py
+++ crm/customer/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import CustomerListView, CustomerCreateView
+from .views import CustomerListView, CustomerCreateView, CustomerUpdateView
 
 app_name = "customer"
 
 urlpatterns = [
     path("list/", CustomerListView.as_view(), name="customer-list"),
     path("", CustomerCreateView.as_view(), name="customer-create"),
-    path('<int:id>/', CustomerCreateView.as_view(), name="customer-update")
+    path('<int:id>/', CustomerUpdateView.as_view(), name="customer-update")
 
 ]
--- crm/customer/views.py
+++ crm/customer/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, CreateView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView
 from .forms import CustomerForm
 from .models import Customer
 from django.urls import reverse
@@ -14,6 +15,21 @@ class CustomerCreateView(CreateView):
     template_name = "customer/customer.html"
     model = Customer
     form_class = CustomerForm
+
+    def form_valid(self, form):
+        return super().form_valid(form=form)
+
+    def get_success_url(self):
+        return reverse("customer:customer-list")
+
+
+class CustomerUpdateView(UpdateView):
+    template_name = "customer/customer.html"
+    form_class = CustomerForm
+
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(Customer, id=id)
 
     def form_valid(self, form):
         return super().form_valid(form=form)
